9754f900400b751d00a289b08db235503903b97c
Ovais Panjwani <opanjwani@microsoft.com>
2025-01-29 21:38:49 -0800

    Followed Controller based Web API tutorial

---
+++ Models/TaskContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
